e79a972b8ab248afada0aac10f5cc126bf49a10c
B1bizka <113232767+B1bizka@users.noreply.github.com>
2022-12-21 21:25:44 +0300

    newf_iles

---
+++ Test.py
@@ -0,0 +1,15 @@
+import unittest
+from main import Calculator
+class Test(unittest.TestCase):
+  def setUp(self):
+    self.calculator = Calculator()
+  def test_addition(self):
+    self.assertEqual(self.calculator.addition(2,2), 4)
+  def test_subtraction(self):
+    self.assertEqual(self.calculator.subtraction(4,2), 2)
+  def test_multiplication(self):
+    self.assertEqual(self.calculator.multiplication(3,2), 6)
+  def test_division(self):
+    self.assertEqual(self.calculator.division(10,2), 5)
+if __name__ == "__main__":
+  unittest.main()
